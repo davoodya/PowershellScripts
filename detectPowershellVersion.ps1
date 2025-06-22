99d2e2bc248ba2f1b61189a70a0531fb590916a4
davoodya <davoodya40@gmail.com>
2025-06-22 23:34:18 +0330

    add detectPowershell Version Script to detect Powershell Core or Windows Powershell

---
+++ detectPowershellVersion.ps1
@@ -0,0 +1,8 @@
+# Check the PowerShell version
+if ($PSVersionTable.PSVersion.Major -ge 7) {
+    Write-Host "This script is running in PowerShell Core (7+)."
+} elseif ($PSVersionTable.PSVersion.Major -eq 5 -and $PSVersionTable.PSVersion.Minor -eq 1) {
+    Write-Host "This script is running in Windows PowerShell (5.1)."
+} else {
+    Write-Host "This script is running in an unsupported version of PowerShell."
+}
